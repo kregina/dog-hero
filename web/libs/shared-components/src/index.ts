7c9f58facb53235e0a510286be104c3e0d54b40f
Kelly Silva <kelly@kellyregina.com.br>
2019-08-15 19:30:34 -0300

    generate shared components

--- web/libs/shared-components/src/index.ts
+++ web/libs/shared-components/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/shared-components.module';
+export * from './lib/card/card.module';
+export * from './lib/shell/shell.module';
--- web/libs/shared-components/src/lib/shared-components.module.ts
+++ web/libs/shared-components/src/lib/shared-components.module.ts
@@ -1,7 +1,10 @@
+import { CardModule } from './card/card.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShellModule } from './shell/shell.module';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  exports: [CardModule, ShellModule]
 })
 export class SharedComponentsModule {}
