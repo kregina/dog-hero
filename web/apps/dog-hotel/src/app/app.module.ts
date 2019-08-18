198a6bc4f462cbf957882c18381ac5f6ba87bf46
Kelly Silva <kelly@kellyregina.com.br>
2019-08-18 19:02:20 -0300

    implement navbar

--- web/apps/dog-hotel/src/app/app.module.ts
+++ web/apps/dog-hotel/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { API_BASE_URL } from '@dog-hero/api';
+import { BootstrapModule } from '@dog-hero/bootstrap';
+import { ShellModule } from '@dog-hero/shared-components';
 import { environment } from '@dog-hotel-env/environment';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShellModule,
+    BootstrapModule
   ],
   providers: [
     { provide: API_BASE_URL, useValue: environment.apiUrl }
--- web/libs/bootstrap/src/lib/bootstrap.module.ts
+++ web/libs/bootstrap/src/lib/bootstrap.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    NgbModule]
 })
 export class BootstrapModule {}
