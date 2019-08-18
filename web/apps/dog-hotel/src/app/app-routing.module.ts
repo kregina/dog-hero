1c9c2974cc62a7cae29ae8bb9e951aa05fd733bb
Kelly Silva <kelly@kellyregina.com.br>
2019-08-18 17:43:51 -0300

    add home component

---
+++ web/apps/dog-hotel/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule)
+  }
+]
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false,
+    anchorScrolling: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- web/apps/dog-hotel/src/app/app.module.ts
+++ web/apps/dog-hotel/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { API_BASE_URL } from '@dog-hero/api';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { API_BASE_URL } from '@dog-hero/api';
 import { environment } from '@dog-hotel-env/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    AppRoutingModule
   ],
   providers: [
     { provide: API_BASE_URL, useValue: environment.apiUrl }
