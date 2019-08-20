4bb3525bfcfbc9c0c59cf6500c66eb4b28def7ab
Kelly Silva <kelly@kellyregina.com.br>
2019-08-19 22:22:44 -0300

    add angular material and cdk

--- web/apps/dog-hotel/src/app/app.module.ts
+++ web/apps/dog-hotel/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { environment } from '@dog-hotel-env/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     ShellModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     { provide: API_BASE_URL, useValue: environment.apiUrl }
